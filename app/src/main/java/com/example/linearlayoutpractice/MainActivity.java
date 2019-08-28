a3ff0be8e40479d535dd85c5f72a26b072b71bc6
pikaChin <derrfgh4563@gmail.com>
2019-08-29 00:56:31 +0800

    complete user information input and the information output

--- app/src/main/java/com/example/linearlayoutpractice/MainActivity.java
+++ app/src/main/java/com/example/linearlayoutpractice/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.linearlayoutpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void printInformation(View v) {
+        EditText fName = findViewById(R.id.firstName);
+        EditText lName = findViewById(R.id.lastName);
+        EditText phoneNum = findViewById(R.id.phone);
+        TextView output = findViewById(R.id.outputText);
+
+        String _fName = fName.getText().toString();
+        String _lName = lName.getText().toString();
+        String _phone = phoneNum.getText().toString();
+
+        if (_fName.length() == 0 || _lName.length() == 0 || _phone.length() == 0) {
+            output.setText("Please input your information");
+        }
+        else {
+            String summary = _fName + " " + _lName + " your phone number is " + _phone;
+            output.setText(summary);
+        }
     }
 }
